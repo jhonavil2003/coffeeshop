7891253878d14a0ac60e860eb12a07602cdd77eb
Jhon Avila <jf.avila@uniandes.edu.co>
2024-04-28 11:26:08 -0500

    Numero total de cafe por tipo

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -10,11 +10,16 @@ import {Cafe} from "../model/Cafe";
 export class CafeListComponent implements OnInit{
   constructor(private cafeService: CafeService) {}
 
-  coffees : Array<Cafe> = [];
+  coffees: Array<Cafe> = [];
+  numberCoffeeOrigen: number = 0;
+  numberCoffeeBlend: number = 0;
 
   getCoffes (){
     this.cafeService.getCoffees().subscribe(coffees => {
       this.coffees = coffees;
+
+      this.numberCoffeeBlend = this.coffees.filter((coffee) => coffee.tipo == 'Blend').length;
+      this.numberCoffeeOrigen = this.coffees.filter((coffee) => coffee.tipo == 'Café de Origen').length;
     });
   }
 
